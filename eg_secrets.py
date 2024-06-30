cc6951d617ef069ad68e81f368cacb2141f1a5c0
John Wilson <johnny@Johns-MBP.cable.virginm.net>
2024-06-30 16:42:57 +0100

    example secrets.py file - eg_secrets.py

---
+++ eg_secrets.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    "ssid": "MYWIFI-SID",
+    "password": "MY-WIFI-PASSWORD",
+    "phone":"+44770001234", # Phone to send example SMS to
+
+    
+    "mqtt_host":"MYMQTT_SERVER.com",
+    "mqtt_username":"MYMQTTUSER",
+    "mqtt_password":"MYMQTTPWD",
+    "mqtt_port":1883,
+
+    "timezone": 0,  # this is offset from UTC
+    "github_token": "abcdefghij0123456789",
+    "adafruit_io_user": "myusername",
+    "adafruit_io_key": "abcdefghij0123456789",
+}
+
